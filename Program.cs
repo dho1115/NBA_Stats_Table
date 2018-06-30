74637a52a805114695dbaf73955bb05d56435e47
David H <davidhorph@hotmail.com>
2018-06-29 23:27:37 -0500

    Updats
    updats

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace NBA_Stats_Sheet
 {
@@ -6,7 +8,32 @@ namespace NBA_Stats_Sheet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stats KevinDurant = new Stats("Kevin Durant", (decimal)26.4, (decimal)6.8, (decimal)5.4);
+
+            //XML Serializer converts the TYPE to XML.
+            XmlSerializer KDStats = new XmlSerializer(typeof(Stats));
+
+            //StreamWriter creates the FILE NAME and FILE DESTINATION.
+            StreamWriter sw = new StreamWriter(@"C:\Users\Owner\Desktop\NBAStats.txt");
+
+            KDStats.Serialize(sw, KevinDurant);
+
+            sw.Close();
+            sw.Dispose();
+
+            XmlSerializer deserialize = new XmlSerializer(typeof(Stats));
+            StreamReader sr = new StreamReader(@"C:\Users\Owner\Desktop\NBAStats.txt");
+            object obj = deserialize.Deserialize(sr);
+            KevinDurant = (Stats)obj;
+            sr.Close();
+            sr.Dispose();
+
+            
+            Console.WriteLine(KevinDurant);
+
+            
+
+            Console.ReadLine();
         }
     }
 }
