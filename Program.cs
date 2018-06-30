f8f9e65bdddef26f61584c68899d65da1745dc51
David H <davidhorph@hotmail.com>
2018-06-30 00:55:29 -0500

    changes
    changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 
@@ -8,30 +9,44 @@ namespace NBA_Stats_Sheet
     {
         static void Main(string[] args)
         {
-            Stats KevinDurant = new Stats("Kevin Durant", (decimal)26.4, (decimal)6.8, (decimal)5.4);
+            //Stats KevinDurant = new Stats("Kevin Durant", (decimal)26.4, (decimal)6.8, (decimal)5.4);
+
+            List<Stats> NBAPlayers = new List<Stats>
+            {
+                new Stats("Kevin Durant", (decimal)26.4, (decimal)6.8, (decimal)5.4),
+                new Stats("Julius Randle", (decimal)16.1, (decimal)8.0, (decimal)2.6),                
+                new Stats("Rajon Rondo", (decimal)8.3, (decimal)4.0, (decimal)8.2),
+                new Stats("Steven Adams", (decimal)13.9, (decimal)9.0, (decimal)1.2),
+                new Stats("Andrew Wiggins", (decimal)17.7, (decimal)4.4, (decimal)2.0)
+            };
+
+            // ***** SERIALIZER *****
 
             //XML Serializer converts the TYPE to XML.
-            XmlSerializer KDStats = new XmlSerializer(typeof(Stats));
+            XmlSerializer NBA = new XmlSerializer(typeof(List<Stats>));
 
             //StreamWriter creates the FILE NAME and FILE DESTINATION.
             StreamWriter sw = new StreamWriter(@"C:\Users\Owner\Desktop\NBAStats.txt");
 
-            KDStats.Serialize(sw, KevinDurant);
+            NBA.Serialize(sw, NBAPlayers);
 
             sw.Close();
-            sw.Dispose();
+            sw.Dispose();                     
 
-            XmlSerializer deserialize = new XmlSerializer(typeof(Stats));
+            // ***** DESERIALIZER *****
+
+            XmlSerializer deserialize = new XmlSerializer(typeof(List<Stats>));
             StreamReader sr = new StreamReader(@"C:\Users\Owner\Desktop\NBAStats.txt");
             object obj = deserialize.Deserialize(sr);
-            KevinDurant = (Stats)obj;
+            List<Stats> XMLList = (List<Stats>)obj;
+
             sr.Close();
             sr.Dispose();
 
-            
-            Console.WriteLine(KevinDurant);
-
-            
+            foreach (var item in XMLList)
+            {
+                Console.WriteLine(item);
+            }                  
 
             Console.ReadLine();
         }
---
+++ Stats.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace NBA_Stats_Sheet
+{
+    [Serializable]
+    public class Stats : ISerializable
+    {
+        public string Player { get; set; }
+        public decimal Points { get; set; }
+        public decimal Rebounds { get; set; }
+        public decimal Assists { get; set; } 
+
+        public Stats(string play = "", decimal pts = 0, decimal reb = 0, decimal asst = 0)
+        {
+            Player = play;
+            Points = pts;
+            Rebounds = reb;
+            Assists = asst;
+        }
+
+        //The constructor below is what the XML Serializer will read.
+
+        
+        public Stats()
+        {
+            
+        } 
+        
+        public override string ToString()
+        {
+            return string.Format("Player: {0} \tPoints: {1} \tRebounds: {2} \tAssists:{3}",
+                Player, Points, Rebounds, Assists);
+        }
+
+
+        /// <summary>
+        /// SERIALIZE data into a separate file.
+        /// </summary>
+        /// <param name="info">Set up "Key Value" pair.</param>
+        /// <param name="context"></param>
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Player: ", Player);
+            info.AddValue("Points: ", Points);
+            info.AddValue("Rebounds: ", Rebounds);
+            info.AddValue("Assists: ", Assists);
+        } 
+
+        /// <summary>
+        /// DESERIALIZE data into something we can read.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        
+        public Stats(SerializationInfo info, StreamingContext context)
+        {
+            info.GetValue("Player: ", typeof(string));
+            info.GetValue("Points: ", typeof(decimal));
+            info.GetValue("Rebounds: ", typeof(decimal));
+            info.GetValue("Assists: ", typeof(decimal));
+        }
+    }
+}
